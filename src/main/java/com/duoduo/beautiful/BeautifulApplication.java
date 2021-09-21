3cdf0a1771f741345e8ce68d3c830fccbb428edf
happyweizai <pwbeautiful@163.com>
2021-09-21 19:40:12 +0800

    第一次提交git

---
+++ src/main/java/com/duoduo/beautiful/BeautifulApplication.java
@@ -0,0 +1,21 @@
+package com.duoduo.beautiful;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class BeautifulApplication {
+
+	public static void main(String[] args) {
+		try{
+			SpringApplication.run(BeautifulApplication.class, args);
+		}catch (Exception e){
+			e.printStackTrace();
+			log.error("启动失败",e);
+		}
+
+	}
+
+}
